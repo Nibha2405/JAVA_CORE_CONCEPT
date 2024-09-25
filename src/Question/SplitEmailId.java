f5ecac78f97b98aea6069a6f51f8fd45bcbc7c60
Nibha2405 <impervious.nibha@gmail.com>
2024-09-26 02:38:16 +0530

    2nd test

--- src/Question/SplitEmailId.java
+++ src/Question/SplitEmailId.java
@@ -7,6 +7,7 @@ public class SplitEmailId {
          System.out.println("My name");
          System.out.println("Hello World");
          System.out.println("Hello Family");
+         System.out.println("Friends");
  }
 
 }
