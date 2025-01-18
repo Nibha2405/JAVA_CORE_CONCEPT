2f2f32e2f7eeab85caf483ed1f611169b14b5ea1
Nibha Kumari <98377632+Nibha2405@users.noreply.github.com>
2025-01-18 21:31:31 +0530

    Create Solution.java

---
+++ src/Question/Solution.java
@@ -0,0 +1,33 @@
+public class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        StringBuilder s = new StringBuilder();
+        int maxLength = Math.max(word1.length(), word2.length());
+
+        for (int i = 0; i < maxLength; i++) {
+            if (i < word1.length()) {
+                s.append(word1.charAt(i));
+            }
+            if (i < word2.length()) {
+                s.append(word2.charAt(i));
+            }
+        }
+        return s.toString();
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        // Example test cases
+        String word1 = "abc";
+        String word2 = "pqr";
+        System.out.println("Merged String: " + solution.mergeAlternately(word1, word2)); // Output: "apbqcr"
+
+        word1 = "ab";
+        word2 = "pqrs";
+        System.out.println("Merged String: " + solution.mergeAlternately(word1, word2)); // Output: "apbqrs"
+
+        word1 = "abcd";
+        word2 = "pq";
+        System.out.println("Merged String: " + solution.mergeAlternately(word1, word2)); // Output: "apbqcd"
+    }
+}
